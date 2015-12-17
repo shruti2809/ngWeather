37153d79a8318438c1ce343fe6daeff7412f2e05
Shruti Sinha <shruti2809@gmail.com>
2015-12-17 16:05:26 +0530

    Set routing path of pages

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+//ROUTES
+weatherApp.config(function ($routeProvider) {
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
+    
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
+})
+
+//SERVICES
+weatherApp.service('cityService', function(){
+    
+    this.city = "Hyderabad, Telangana";
+    
+})
+
+//CONTROLLER
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function(){
+        
+        cityService.city = $scope.city;
+    });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', $'resource', 'cityService', function($scope, $resource, cityService) {
+    
+     $scope.city = cityService.city;
+                                             
+     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily")                                       
+    
+}]);
